a79b6e1363f703e1a670e269466863aae1dea670
gcp <gcp@gossa.eu>
2020-07-27 17:18:08 +0200

    Terraform migration from version 0.11 to version 0.12

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -18,7 +18,7 @@ locals {
 }
 
 resource "google_compute_firewall" "allow-http" {
-  name    = concat(local.network "-allow-http")
+  name    = concat(local.network, "-allow-http")
   network = local.network
   project = var.project
 
